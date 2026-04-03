ef6940081b1fa469ff323108b17e685736450701
Anup Narayan Ojha <anuplynn88@gmail.com>
2026-04-03 18:34:13 +0530

    Fixed vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mantra-jaap-tracker/', // Ye line add karni hai
+  base: '/mantra-jaap-tracker/', // <-- Dhyan rahe, aage aur peeche dono taraf slash (/) hona zaroori hai!
 })
